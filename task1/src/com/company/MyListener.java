ff480cc63e9669ff5a45d2dae465977bd213cb5c
Michael <55315647+Karambasss@users.noreply.github.com>
2021-02-17 19:46:21 +0300

    Delete task1 directory

--- task1/src/com/company/MyListener.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class MyListener implements ChangeListener{
-
-    @Override
-    public void onChange(OvservableStringBuilder stringBuilder) {
-        System.out.println("CHANGED: " + stringBuilder.toString());
-    }
-}
