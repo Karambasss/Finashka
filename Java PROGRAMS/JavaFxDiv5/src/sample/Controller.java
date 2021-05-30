2a46693d0d9ed40a1a070dd29d05d5e1bf24b08f
Michael <55315647+Karambasss@users.noreply.github.com>
2021-05-31 01:47:08 +0300

    Решение задачи с делением на 5 двухзначных чисел

---
+++ Java PROGRAMS/JavaFxDiv5/src/sample/Controller.java
@@ -0,0 +1,88 @@
+package sample;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    TextField textField;
+    @FXML
+    TextField textField1;
+    @FXML
+    TextArea resultField;
+
+    @FXML
+    public void getDiv5(){
+        resultField.setVisible(false);
+        try {
+            int firstNum = Integer.parseInt(textField.getText());
+            int secondNum = Integer.parseInt(textField1.getText());
+
+            //первая проверка - на отрицательные диапазоны
+            if (firstNum < 0 && secondNum < 0){
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Вы ввели два отрицательных диапазона!");
+                alert.showAndWait();
+                textField.clear();
+                textField1.clear();
+            }
+            //вторая - на ввод диапазона в порядке возрастания, а не убывания
+            else if (secondNum > firstNum){
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Вы ввели диапазон в порядке возрастания!");
+                alert.showAndWait();
+                textField.clear();
+                textField1.clear();
+            }
+//            else if (firstNum == secondNum && firstNum >= 10 && secondNum >= 10){
+//                if (firstNum % 5 == 0){
+//                    resultField.setVisible(true);
+//                    resultField.setText("Ответ: " +  firstNum);
+//                }
+//                textField.clear();
+//                textField1.clear();
+//            }
+            else if (firstNum >= secondNum && firstNum >= 10 && secondNum >= 10){
+                resultField.clear();
+                ArrayList<Integer> resultList = new ArrayList<>();
+                for (int i = firstNum; i >= secondNum; i--){
+                    if (i % 5 == 0){
+                        resultList.add(i);
+                    }
+                }
+                if (resultList.size() > 0){
+                    resultField.setVisible(true);
+                    resultField.setText("Ответ: " + resultList.toString());
+                }
+                else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR,"Не нашлись числа, которые делятся на 5 без остатка!");
+                    alert.showAndWait();
+                }
+                textField.clear();
+                textField1.clear();
+            }
+            //проверка на однозначность числа и его положительность
+            else if ((firstNum > 0 && firstNum < 10)  || (secondNum > 0 && secondNum < 10)){
+                Alert alert = new Alert(Alert.AlertType.ERROR,"Введите двузначное число!");
+                alert.showAndWait();
+                textField.clear();
+                textField1.clear();
+            }
+            else if (secondNum < 0){
+                Alert alert = new Alert(Alert.AlertType.ERROR,"Введите положительное число!");
+                alert.showAndWait();
+                textField.clear();
+                textField1.clear();
+            }
+        }
+        catch (NumberFormatException exception){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Вы допустили ошибку ввода данных!");
+            alert.showAndWait();
+            textField.clear();
+            textField1.clear();
+        }
+    }
+}
---
+++ Java PROGRAMS/JavaFxDiv5/tests/sample/ControllerTest.java
@@ -0,0 +1,24 @@
+package sample;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDiv5() {
+    }
+}
