74c5cc786949bbc5dbbb1256cc12c9bcd6eceab3
Michael <55315647+Karambasss@users.noreply.github.com>
2021-06-14 00:13:05 +0300

    Решение задания о нахождении файлов в указанной пользователем директории

--- Java PROGRAMS/JavaFxFindDirectory/src/sample/Controller.java
+++ Java PROGRAMS/JavaFxFindDirectory/src/sample/Controller.java
@@ -7,24 +7,30 @@ import javafx.scene.control.TextArea;
 import java.io.File;
 import java.util.ArrayList;
 
-
+/**
+ * Данный класс создан для работы с javaFx
+ * @author Кузнецов Михаил Пи19-4
+ */
 public class Controller {
     @FXML
     TextArea path_fld;
     @FXML
     TextArea result_fld;
 
+    /**
+     * Данный метод обрабатывает нажатие на кнопку, отдает во view пользователя список файлов, лежащих в указанной пользователем директории
+     */
     @FXML
     public void clickBtn(){
         result_fld.clear();
-        File file = new File(path_fld.getText());
-        if (file.isDirectory()){
+        File file = new File(path_fld.getText()); // path_fld - текстареа, где пользователь задает путь
+        if (file.isDirectory()){ // если файл является директорией
             ArrayList<String> arrayList = new ArrayList<>();
             for (String i : file.list()){
                 //arrayList.add(file.getAbsolutePath() + File.separator + i + '\n');
                 arrayList.add(file.getAbsolutePath() + File.separator + i + '\n');
             }
-            if (arrayList.size() != 0){
+            if (arrayList.size() > 0){
                 result_fld.setText(arrayList.toString());
             }
             else {
--- Java PROGRAMS/JavaFxFindDirectory/src/sample/Main.java
+++ Java PROGRAMS/JavaFxFindDirectory/src/sample/Main.java
@@ -6,8 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Главный класс, который запускает javaFx и задает FXML
+ * @author Кузнецов Михаил Пи19-4
+ */
 public class Main extends Application {
-
+    /***
+     * Данный метод настраивает и запускает javaFx.
+     * @param primaryStage Главное окно приложения
+     * @throws Exception Ошибка старта программы
+     */
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -15,8 +23,10 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
-
+    /**
+     * Основной метод, который вызывается при запуске программы.
+     * @param args массив аргументов с которыми может запускаться программа.
+     */
     public static void main(String[] args) {
         launch(args);
     }
