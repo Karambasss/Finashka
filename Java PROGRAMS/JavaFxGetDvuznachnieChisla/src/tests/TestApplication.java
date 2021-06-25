769889d97135e5387a9e7ac234ddf427732cfec9
Michael <55315647+Karambasss@users.noreply.github.com>
2021-06-25 18:47:30 +0300

    Напишите программу, которая выводит на экран все положительные двузначные числа
    Напишите программу, которая выводит на экран все положительные двузначные числа (в возрастающем порядке). Диапазон чисел задает пользователь. Приложение должно быть с графическим интерфейсом JavaFX. Написать тест к этому приложению на основе JUnit 5.

---
+++ Java PROGRAMS/JavaFxGetDvuznachnieChisla/src/tests/TestApplication.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.junit.jupiter.api.*;
+import sample.Controller;
+
+import java.util.ArrayList;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TestApplication {
+    private Controller controller;
+    @BeforeAll
+    public void beforeTesting(){
+        controller = new Controller();
+    }
+    @Test
+    public void testGetYourNumber(){
+        ArrayList<Integer> arrayList = controller.getYourNumbers(100,10);
+        Assertions.assertFalse(arrayList == null);
+        System.out.println("Успешно пройден тест");
+    }
+    @AfterAll
+    public void afterTesting(){
+        controller = null;
+    }
+}
